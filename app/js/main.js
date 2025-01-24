32b5733ac53e43568de00ff155f94ca02077bb86
sergey-komar <kursev81@yandex.ru>
2025-01-24 19:13:43 +0300

    home and news

--- app/js/main.js
+++ app/js/main.js
@@ -65,13 +65,19 @@ window.addEventListener('DOMContentLoaded', () => {
             }
         }
     }
-
+  
     setClock('.timer', deadline);
+
+        
+    
+   
 
 
 
     // Маска для Инпута
     var element = document.getElementById('input-mask');
+    var element1 = document.getElementById('input-mask1');
+    
     var maskOptions = {
         mask: '+{7}(000)000-00-00'
     };
@@ -79,7 +85,103 @@ window.addEventListener('DOMContentLoaded', () => {
     if(element){
         var mask = IMask(element, maskOptions);
     }
-    
-   
+    if(element1){
+        var mask1 = IMask(element1, maskOptions);
+    }
+
+
+   const accardionBtn = document.querySelectorAll('.questions-accardion__btn');
+
+    accardionBtn.forEach(item => {
+        item.addEventListener('click', () => {
+            item.classList.toggle('open');
+        })
+    })
+
+
+
+    const btn = document.querySelector('.btnUp');
+    const heading = document.querySelector('.main');
+
+    if(document && heading){
+      document.addEventListener('scroll', () => {
+          if(scrollY > heading.offsetHeight){
+              btn.classList.add('btnUp-visible');
+          }else{
+              btn.classList.remove('btnUp-visible');
+          }
+          
+      });
+  }
+  
+  if(btn){
+      btn.addEventListener('click', ()=>{
+          window.scrollTo({
+            top: 0, 
+            behavior: 'smooth' 
+        
+          });
+        });
+  }
+
+
+        const modalBtn = document.querySelectorAll('.btn-modal');
+        const modal = document.querySelector('.modal');
+        const modalCloseBtn = document.querySelector('.modal__close');
+        const test = calcScroll();
+
+
+            modalBtn.forEach(btn =>{
+                btn.addEventListener('click', ()=>{
+                    modal.classList.add('show');
+                    modal.classList.remove('hide');
+                    document.body.style.overflow = 'hidden';
+                    document.body.style.marginRight = `${test}px`;
+                })
+            });
+
+            modalCloseBtn.addEventListener('click', ()=>{
+                    modal.classList.add('hide');
+                    modal.classList.remove('show');
+                    document.body.style.overflow = '';
+                    document.body.style.marginRight = `0px`;
+            });
+
+            modal.addEventListener('click', (e)=>{
+                if(e.target == modal){
+                    modal.classList.add('hide');
+                    modal.classList.remove('show');
+                    document.body.style.overflow = '';
+                    document.body.style.marginRight = `0px`;
+                }
+            });
+
+            document.addEventListener('keydown', (e)=>{
+                if(e.code == 'Escape' && modal.classList.contains('show')){
+                    modal.classList.add('hide');
+                    modal.classList.remove('show');
+                    document.body.style.overflow = '';
+                    document.body.style.marginRight = `0px`;
+                }
+            })
+
+        //УБИРАЕМ ДЁРГАНИЕ МОДАЛЬНОГО ОКНА ПРИ ПОЯВЛЕНИИ
+        function calcScroll(){
+            let div = document.createElement('div');
+            div.style.width = '50px';
+            div.style.height = '50px';
+            div.style.overflowY = 'scroll';
+            div.style.visibility = 'hidden';
+            
+            document.body.append(div);
+
+            let scrollWidth = div.offsetWidth - div.clientWidth;
+            
+            div.remove();
+            
+            return scrollWidth;
+            }
+            //УБИРАЕМ ДЁРГАНИЕ МОДАЛЬНОГО ОКНА ПРИ ПОЯВЛЕНИИ
+
 
 })
